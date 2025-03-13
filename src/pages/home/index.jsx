eb9e829b0681277fbd47496de63af79aa16b3946
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-03-13 13:55:35 -0300

    Responsividade

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -21,9 +21,9 @@ function Home() {
   ]
 
   return (
-    <div className="flex items-center pt-25 flex-col">
-      <form className='flex flex-col gap-8 p-8 rounded-[10px] bg-form max-w-[50%] w-[400px] mb-5'>
-        <h1 className='text-white text-3xl'>Cadastro de Usuário</h1>
+    <div className="flex items-center justify-center min-h-screen flex-col">
+      <form className='flex flex-col gap-8 p-8 rounded-[10px] bg-form w-[400px] mb-5'>
+        <h1 className='text-white text-3xl max-sm:text-[18px] text-center'>Cadastro de Usuário</h1>
         <input name='nome' type='text' placeholder='Digite seu Nome' />
         <input name='idade' type='number' placeholder='Digite sua idade'/>
         <input name='email' type='email' placeholder='Digite seu Email' />
@@ -37,7 +37,7 @@ function Home() {
               <p>Idade: <span>{user.age} </span></p>
               <p>Email: <span>{user.email}</span></p>
             </div>
-            <button className='cursor-pointer text-red-700 text-[20px] duration-100 hover:opacity-80'>
+            <button className='cursor-pointer text-red-700 text-[20px] duration-100 max-sm:ml-10 hover:opacity-80'>
               <FontAwesomeIcon icon={faTrash} />
             </button>
           </div>
